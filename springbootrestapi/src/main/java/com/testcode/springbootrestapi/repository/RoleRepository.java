9b151e4b3f76955a214410a17f6127c2be621ce6
Abhilash C <30768127+abhilashc708@users.noreply.github.com>
2019-07-17 23:52:30 +0530

    crud operation in user details
    crud operation in user details

--- springbootrestapi/src/main/java/com/testcode/springbootrestapi/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.testcode.springbootrestapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.testcode.springbootrestapi.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
