03c16bce4afcaa8d8edb1b3fc334cb6fff18f7dc
Araf <arafullata@gmail.com>
2019-07-20 17:00:46 +0530

    relocate to bse directory

--- springbootrestapi/src/main/java/com/testcode/springbootrestapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.testcode.springbootrestapi.repository;
-
-import com.testcode.springbootrestapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
